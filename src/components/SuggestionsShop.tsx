1652d6721d6aa2364d05bf7782015107381cf387
Lydia Law <lydialawli@gmail.com>
2022-02-16 22:31:28 +0000

    group by visited boolean

--- src/components/SuggestionsShop.tsx
+++ src/components/SuggestionsShop.tsx
@@ -14,12 +14,13 @@ export default function CountrySelect() {
       id="country-select-demo"
       sx={{ width: 300 }}
       options={stores}
+      groupBy={(shop) => (shop.isVisited ? 'Visited' : 'Not visited yet')}
       autoHighlight
-      getOptionLabel={(option) => option.name}
-      renderOption={(props, option) => (
+      getOptionLabel={(shop) => shop.name}
+      renderOption={(props, shop) => (
         <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
           <img loading="lazy" width="20" src={storePng} alt="" />
-          {option.name}
+          {shop.name}
         </Box>
       )}
       renderInput={(params) => <TextField {...params} label="Choose a shop" />}
